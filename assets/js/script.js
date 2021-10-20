c984023b0646e91c83336eb6e07c5469a9641fc7
Morgan Sherrill <mo.sherrill@outlook.com>
2021-10-20 16:30:26 -0700

    simple fetch for the artist

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,16 @@ fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
 .catch(err => {
 	console.error(err);
 });
+
+let artist = "bob dylan"
+let requestUrl = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      
+      })
