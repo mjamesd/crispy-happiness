a31c902b2d443ca36feff3c6e5c340dcbe0ea307
Morgan Sherrill <mo.sherrill@outlook.com>
2021-10-21 23:39:03 -0700

    bringing my js file back to what it was before i made any changes as not to mess with that for this pull request

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,3 @@ fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
 .catch(err => {
 	console.error(err);
 });
-
-var artistID
-let artist = "bob dylan"
-let requestUrl = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-	  console.log(data.artists[0].idArtist)
-		let artistID = data.artists[0].idArtist
-		return artistID
-			
-		})
-
-		console.log(artistID)
