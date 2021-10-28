125a937e83cf442ad48f4fa883c0fb65da42ed74
jefrey Colegrove <JefreyColegrove@gmail.com>
2021-10-27 21:25:40 -0700

    add show/hide functionality to discography

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -41,6 +41,7 @@ const artistLastFmEl = $("#artistLastFm");
 const artistTwitterEl = $("#artistTwitter");
 const artistFacebookEl = $("#artistFacebook");
 const artistInfoEl = $("#artistInfo");
+const showMoreEl = $("#showMore")
 
 // Names of dynamically-created elements
 const artistBioText = "artistBioText";
@@ -62,7 +63,8 @@ const localStorageEntity = "cph-";
 function displayBio(thisArtist) {
     artistBioEl.empty();
     artistBioEl.append($(imgEl).attr("src", thisArtist.strArtistBanner).attr("alt", `${thisArtist.strArtist} banner image`).attr("class", "responsive-img"));
-    artistBioEl.append($(divEl).attr("id", artistBioText).text(`${thisArtist.strBiographyEN.substr(0, 200)}... (read more)`));
+    artistBioEl.append($(divEl).attr("id", artistBioText).text(`${thisArtist.strBiographyEN.substr(0, 200)}... (read more)`).on("click", function () {
+    }));
     // Jefrey -- add jQuery UI widget 'dialog' to display all bio text when "read more" is clicked... see line 81 & 82
 }
 
@@ -108,6 +110,10 @@ function displayDiscography(artistId) {
             if (index == 5) {
                 artistDiscographyListEl.append($(liEl).html($(aEl).attr("id", "seeMoreDiscography").text("see more")));
                 // TO DO: Add .click event to show all albums
+                $("#seeMoreDiscography").on("click", function () {
+                    $(".displayNoneOnLoad").show()
+                    $("#seeMoreDiscography").hide()
+                })
             }
             // By default, only show the first five albums (most popular) but write all of them to the page
             if (index >= 5) {
@@ -115,6 +121,10 @@ function displayDiscography(artistId) {
             }
             artistDiscographyListEl.append(thisAlbum);
         }
+        artistDiscographyListEl.append($(liEl).html($(aEl).attr("class", "displayNoneOnLoad").text("see less").on("click", function(){
+            $(".displayNoneOnLoad").hide()
+            $("#seeMoreDiscography").show()
+        })))
     });
 }
 
@@ -146,3 +156,12 @@ searchBtnEl.click(() => {
         displayDiscography(artistInfo.idArtist);
     });
 });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+const testimgEl = "https://lorempixel.com/250/250/nature/5"
+
+  $(".carousel").append(aEl).attr("class", "carousel-item").attr("href", "#five!").prepend('<img id="theImg" src="https://lorempixel.com/250/250/nature/5" />')
+
+// artistFacebookEl.html($(imgEl).src(data[0]));
